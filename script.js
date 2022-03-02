136824f26332811b987642c8af6118bcbe32eff1
Divyanshu Dhruv <71079602+divyanshudhruv@users.noreply.github.com>
2022-03-02 09:44:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+      $("html").mousemove(function (e) {
+        var wx = $(window).width();
+        var wy = $(window).height();
+
+        var x = e.pageX - this.offsetLeft;
+        var y = e.pageY - this.offsetTop;
+
+        var newx = x - wx / 2;
+        var newy = y - wy / 2;
+
+        $("span").text(newx + ", " + newy);
+
+        $(".nav").each(function () {
+          var speed = $(this).attr("data-speed");
+          if ($(this).attr("data-revert")) speed *= 1;
+          TweenMax.to($(this), 1, { x: -1 - newx * speed, y: -1 - newy * speed });
+        });
+      });
